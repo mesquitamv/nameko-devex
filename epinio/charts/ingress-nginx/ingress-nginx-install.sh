4dbf016c5161e5018da8d4d1c3f89d813c407311
Marcus Mesquita <marcus.mesquita@totvs.com.br>
2023-02-27 10:38:09 -0300

    Adjust ingress-nginx-install.sh

--- epinio/charts/ingress-nginx/ingress-nginx-install.sh
+++ epinio/charts/ingress-nginx/ingress-nginx-install.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
-
 # Install NGINX
 helm upgrade \
   --install ingress-nginx ingress-nginx \
   --repo https://kubernetes.github.io/ingress-nginx \
   --namespace ingress-nginx \
   --create-namespace \
-  --set controller.ingressClassResource.default=true
+  --set controller.ingressClassResource.default=true	
